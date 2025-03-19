b7e160dd96505b2ba6ff496aeeb4b38bb211d56a
Geronimo Sorzzoni <ing.gsorzzoni@gmail.com>
2025-03-19 12:53:14 -0300

    add exception handler

---
+++ src/main/java/com/greatstocksdata/config/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.greatstocksdata.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, Object> body = Map.of(
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "message", ex.getMessage()
+        );
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
